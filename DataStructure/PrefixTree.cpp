389626ed233ee2c6785b06cecc4609bddab20a18
yxc <136000550@qq.com>
2024-06-10 20:12:56 +0800

    使用PrefixTree代替了Trie,使用模板参数初始化代替了形参初始化字典树

--- DataStructure/PrefixTree.cpp
+++ DataStructure/PrefixTree.cpp
@@ -1,6 +1,5 @@
-
 /*
- * Trie(字典树)
+ * PrefixTree(字典树)
  * 设计思想：字典树设计的思想是基于动态开点的m叉树。
  * 基于面向对象的编程思想，本方法尽可能多的隐藏了内部实现的细节，并且将必要的编程接口暴露在外部，并需要对这些接口进行直接的修改。
  * 在该设计中，基础的方法都已经实现，建议用户根据自己的需求自己在字典树主体或者节点类型中实现对应的逻辑。
@@ -21,44 +20,39 @@
 
 
 
-template<const unsigned int ALPHABET_SIZE> class Trie;
 /*
-  Trie类的节点，每个节点有若干个指针，在实例化类对象时指定。
+  字典树的树节点结构。
   每个节点除了指针还有两个变量来记数，分别表示经过当前节点的数据数量pass_count_和以当前节点为结尾的数据数量end_count_。
 */
 template<unsigned int ALPHABET_SIZE>
-class TrieNode {
-    friend class Trie<ALPHABET_SIZE>;
+class PrefixTreeNode {
 public:
-    TrieNode() : pass_count_(0), end_count_(0), next_{} {}
-
-private:
+    PrefixTreeNode() : pass_count_(0), end_count_(0), next_{} {}
     unsigned int  pass_count_;
     unsigned int  end_count_;
-
     std::array<int, ALPHABET_SIZE> next_;
 };
 
 
 
 /*
-  字典树，实例化时需要传递一个非负值作为节点的指针数量，初始化必须携带起始字符，如果是int的二进制数据建树初始化列表可以输入0。
-  使用示例：Trie<26> trie('a'); Trie<2> trie(0);
-  注意：如果将数字作为二进制来存储到字典树中，只支持int型。
+  字典树，实例化时需要指定起始字符和节点中孩子数量。
+  使用示例：Trie trie; vector<Trie> trie(2);
+  注意：使用前务必在类的末尾处指定起始字符和数节点中孩子数量。
 */
 
-template<const unsigned int ALPHABET_SIZE>
-class Trie {
+template<typename START_ALPHABET, const unsigned int ALPHABET_SIZE>
+class PrefixTree {
 public:
-    Trie(char start) : start_(start), root_(0) {
+    PrefixTree() : root_(0) {
         trie_.resize(1);
     }
 
+    using Type = decltype(START_ALPHABET::value);
 
-    /*
-      将int型变量以二进制形式插入到字典树中。
-    */
-    void insert(int x) {
+
+    /* 将int型变量以二进制形式插入到字典树中。 */
+    void insert(const int& x) {
         int cur = root_;
         for (int i = 30; i >= 0; --i) {
             int p = (x >> i) & 1;
@@ -72,10 +66,8 @@ public:
         trie_[cur].end_count_ ++;
     }
 
-    /*
-      从字典树中移除二进制形式的int型变量值。
-    */
-    void erase(int x) {
+    /* 从字典树中移除二进制形式的int型变量值。 */
+    void erase(const int& x) {
         int cur = root_;
         for (int i = 30; i >= 0; --i) {
             int p = (x >> i) & 1;
@@ -87,11 +79,8 @@ public:
         trie_[cur].end_count_ --;
     }
 
-    /*
-      获取字典树中异或最大值。
-    */
-
-    int getMaxXor(int x) {
+    /* 获取字典树中异或最大值。 */
+    int getMaxXor(const int& x) {
         int cur = root_;
         int res = 0;
         for (int i = 30; i >= 0; --i) {
@@ -110,13 +99,11 @@ public:
         return res;
     }
 
-    /*
-      字典树中插入字符串。
-    */
+    /* 字典树中插入字符串。 */
     void insert(const std::string& s) {
         int cur = root_;
         for (const auto& x : s) {
-            int p = x - start_;
+            int p = x - getStartAlpha();
             if (trie_[cur].next_[p] == 0) {
                 trie_[cur].next_[p] = (int)trie_.size();
                 trie_.emplace_back();
@@ -128,13 +115,11 @@ public:
         trie_[cur].end_count_ ++;
     }
 
-    /*
-      从字典树中移除字符串。
-    */
+    /* 从字典树中移除字符串。*/
     void erase(const std::string& s) {
         int cur = root_;
         for (const auto& x : s) {
-            int p = x - start_;
+            int p = x - getStartAlpha();
             cur = trie_[cur].next_[p];
             assert(trie_[cur].pass_count_ > 0);
             trie_[cur].pass_count_ --;
@@ -143,18 +128,14 @@ public:
         trie_[cur].end_count_ --;
     }
 
-    /*
-      统计字典树中s的数量。
-    */
+    /* 统计字典树中s的数量。 */
     template<typename T>
     int countUnique(const T& s) {
         int cur = getLastPointer(s);
         return cur == 0 ? cur : trie_[cur].end_count_;
     }
 
-    /*
-      统计字典树中前缀包含s的数量。
-    */
+    /* 统计字典树中前缀包含s的数量。 */
     template<typename T>
     int countPrefix(const T& s) {
         int cur = getLastPointer(s);
@@ -164,11 +145,11 @@ public:
 
 
 private:
-    char start_;
     int  root_;
 
-    std::vector<TrieNode<ALPHABET_SIZE>> trie_;
-
+    std::vector<PrefixTreeNode<ALPHABET_SIZE>> trie_;
+    
+    constexpr static Type getStartAlpha() { return  START_ALPHABET::value;}
 
     inline int getLastPointer(const int& x) {
         int cur = root_;
@@ -185,7 +166,7 @@ private:
     inline int getLastPointer(const std::string& s) {
         int cur = root_;
         for (const auto& x : s) {
-            int p = x - start_;
+            int p = x - getStartAlpha();
             if (trie_[cur][p] == 0) {
                 return 0;
             }
@@ -194,3 +175,9 @@ private:
         return cur;
     }
 };
+
+// constexpr char START_ALPHABET = 'a';
+// constexpr int TRIE_NODE_SIZE = 26;
+constexpr int START_ALPHABET = 0;
+constexpr int TRIE_NODE_SIZE = 2;
+using Trie = PrefixTree<std::integral_constant<decltype(START_ALPHABET), START_ALPHABET>, TRIE_NODE_SIZE>;
