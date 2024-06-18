220507412937111ef5e204f634da0bb0f3a5c25f
yxc <136000550@qq.com>
2024-06-18 21:31:22 +0800

    tarjan结构化，直接根据邻接表创建

--- Graph/Tarjan.cpp
+++ Graph/Tarjan.cpp
@@ -1,6 +1,10 @@
-
 /*
- * Tarjan()
+ * Tarjan
+ * 设计思想：目前只实现强连通分量查询，基于有向图的遍历过程中维护的栈与访问次序来判定连通分量。
+ * 基于面向对象的编程思想，本方法尽可能多的隐藏了内部实现的细节，并且将必要的编程接口暴露在外部，并需要对这些接口进行直接的修改。
+ *                             使用方法: 将图数据作为模板参数传进来，只支持vector实现的二维邻接表。
+ *                                      节点起始下标可以根据模板参数指定0或者1，构建Tarjan时可以使用std::move()。
+ *
  * gitHub(仓库地址): https://github.com/yxc-s/programming-template.git
  */
 
@@ -9,25 +13,22 @@
 
 
 
-
-
-
-/*
-  约定图树节点下标总是从1开始！！
-*/
+template<const unsigned int START_INDEX> 
 class Tarjan{
 public:
-    Tarjan(const vector<vector<int>>& s):
-        n_((int)s.size() - 1),
+    Tarjan(std::vector<std::vector<int>>&& s):
         al_(s),
+        n_(al_.size() - 1),
+        timer_(0),
         low_(n_ + 1, -1),
         dfn_(n_ + 1, -1),
-        in_stack_(n_ + 1, false),
-        timer_(0){}
+        in_stack_(n_ + 1, false)
+        {}
 
-    //strongly connected component
-    std::vector<vector<int>> getScc(){
-        for (int i = 1; i <= n_; ++i){
+    
+    /* 获取所有的连通分量 */
+    const std::vector<std::vector<int>>& getScc(){
+        for (int i = START_INDEX; i <= n_; ++i){
             if (dfn_[i] == -1){
                 dfsScc(i);
             }
@@ -37,14 +38,15 @@ public:
 
 
 private:
-    int n_;
-    std::vector<vector<int>> al_;
-    std::vector<vector<int>> components_;
-    std::vector<int> low_;
-    std::vector<int> dfn_;
-    std::vector<bool> in_stack_;
-    std::stack<int> stk_;
-    int timer_;
+    size_t                           n_;
+    size_t                           timer_;
+    std::vector<std::vector<int>>    al_;
+    std::vector<std::vector<int>>    components_;
+    std::vector<int>                 low_;
+    std::vector<int>                 dfn_;
+    std::vector<bool>                in_stack_;
+    std::stack<int>                  stk_;
+    
 
     void dfsScc(int u){
         low_[u] = dfn_[u] = timer_ ++;
@@ -53,13 +55,12 @@ private:
         for (const auto& v : al_[u]){
             if (dfn_[v] == -1){
                 dfsScc(v);
-                low_[u] = min(low_[u], low_[v]);
+                low_[u] = std::min(low_[u], low_[v]);
             }
-            else if (in_stack_[v] == true){
-                low_[u] = min(low_[u], low_[v]);
+            else if (in_stack_[v]){
+                low_[u] = std::min(low_[u], low_[v]);
             }
         }
-        //head of strongly connected component
         if (low_[u] == dfn_[u]){
             components_.emplace_back();
             while (!stk_.empty()){
@@ -73,6 +74,7 @@ private:
             }
         }
     }
-
-
 };
+
+
+using Tarj = Tarjan<1>;
